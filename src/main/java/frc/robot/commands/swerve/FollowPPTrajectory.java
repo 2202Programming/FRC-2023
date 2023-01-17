c9b2a1b8ea88e967ea616afb7e2cf0a4d90149d6
Nathanael Ren <nren2013@gmail.com>
2023-01-16 20:53:56 -0600

    UNTESTED: swerve command / subsystem separation

--- src/main/java/frc/robot/commands/swerve/FollowPPTrajectory.java
+++ src/main/java/frc/robot/commands/swerve/FollowPPTrajectory.java
@@ -1,7 +1,10 @@
 package frc.robot.commands.swerve;
 
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+import com.pathplanner.lib.PathPoint;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -13,6 +16,8 @@ import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Sensors_Subsystem;
 import frc.robot.subsystems.SwerveDrivetrain;
 
 public class FollowPPTrajectory extends CommandBase {
@@ -93,6 +98,72 @@ public class FollowPPTrajectory extends CommandBase {
       new InstantCommand(m_robotDrive::stop),
       new PrintCommand("***Done Running Path ")
       );
+  }
+
+  public static PathPlannerTrajectory getPPPath(String pathName) {
+    return PathPlanner.loadPath(pathName, 1, 1); //last two parameters are max velocity and max accelleration
+  }
+
+  /*
+   * @param boolean useOdo when true, use odometry's current robot position.
+   *                       when false, use the path's starting point as the true robot position.
+   *         First auto path (or when going from a known position), useOdo should be false, otherwise true
+   */
+  public static Command pathFactoryAuto(PathConstraints constraints, String pathname, boolean useOdo){
+    SwerveDrivetrain sdt = RobotContainer.RC().drivetrain;
+    Sensors_Subsystem sensors = RobotContainer.RC().sensors;
+
+    var path = PathPlanner.loadPath(pathname, constraints);
+
+    if (path == null) {
+      return new InstantCommand();  // no path selected
+    }
+    
+    sdt.m_field.getObject(pathname).setTrajectory(path);
+
+    // get initial state from the trajectory
+    PathPlannerState initialState = path.getInitialState();
+    Pose2d startingPose = new Pose2d(initialState.poseMeters.getTranslation(), initialState.holonomicRotation);
+    //TODO: take PID values from Constants or construction?
+    PIDController xController = new PIDController(4.0, 0.0, 0.0, Constants.DT);   // [m]
+    PIDController yController = new PIDController(4.0, 0.0, 0.0, Constants.DT);   // [m]
+    PIDController thetaController = new PIDController(4, 0, 0, Constants.DT);     // [rad]
+      //Units are radians for thetaController; PPSwerveController is using radians internally.
+      thetaController.enableContinuousInput(-Math.PI, Math.PI); //prevent piroutte paths over continuity
+
+      PPSwerveControllerCommand swerveControllerCommand =
+      new PPSwerveControllerCommand(
+          path,
+          sdt::getPose, // Functional interface to feed supplier
+          sdt.getKinematics(),
+          // Position controllers 
+          xController,
+          yController,
+          thetaController,
+          sdt::drive,
+          sdt
+      );
+
+
+    // Run path following command, then stop at the end.
+    return new SequentialCommandGroup(
+      new InstantCommand(()-> {
+        if (!useOdo) // useOdo is false, starting pose is position/heading as defined in path. Otherwise go from where we are
+          sensors.setAutoStartPose(startingPose);
+      }),
+      new PrintCommand("***Factory: Running Path " + pathname),
+      swerveControllerCommand,
+      new InstantCommand(sdt::stop),
+      new PrintCommand("***Done Running Path " + pathname)
+      );
+  }
+
+  public static PathPlannerTrajectory pathFactoryTele(PathConstraints constraints, Pose2d finalPoint) {
+    SwerveDrivetrain sdt = RobotContainer.RC().drivetrain;
+    //TODO: actually figure out max vel / accel and move to constants
+    return PathPlanner.generatePath(constraints, 
+                          new PathPoint(sdt.getPose().getTranslation(), finalPoint.getRotation(), sdt.getPose().getRotation()), 
+                          new PathPoint(finalPoint.getTranslation(), finalPoint.getRotation(), finalPoint.getRotation()));
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
@@ -4,20 +4,8 @@
 
 package frc.robot.subsystems;
 
-import com.pathplanner.lib.PathConstraints;
-import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.PathPoint;
-import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
-import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 import com.revrobotics.CANSparkMax;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -27,18 +15,18 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.Constants.CAN;
 import frc.robot.Constants.ChassisConfig;
 import frc.robot.Constants.DriveTrain;
 import frc.robot.Constants.NTStrings;
 import frc.robot.Constants.WheelOffsets;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Sensors_Subsystem.EncoderID;
 import frc.robot.util.ModMath;
 import frc.robot.util.PoseMath;
@@ -253,7 +241,7 @@ public class SwerveDrivetrain extends SubsystemBase {
     // update pose
     old_pose = m_pose;
     m_pose = m_odometry.update(sensors.getRotation2d(), meas_pos);
-
+//TODO: get the math out of here
     // from -PI to +PI
     double temp = Math.atan2(m_pose.getY() - old_pose.getY(), m_pose.getX() - old_pose.getX());
     if(temp != 0){ //remove singularity when moving too slow - otherwise lots of jitter
@@ -312,7 +300,7 @@ public class SwerveDrivetrain extends SubsystemBase {
     m_pose = new_pose;
     m_odometry.resetPosition(sensors.getRotation2d(), meas_pos, m_pose);
   }
-
+//TODO: do we REALLY think this is where we need to go? field coords???
   // resets X,Y, and set current angle to be 0
   public void resetPose() {
     m_pose = new Pose2d(0, 0, new Rotation2d(0));
@@ -332,7 +320,7 @@ public class SwerveDrivetrain extends SubsystemBase {
   public void printPose(){
     System.out.println("***POSE X:" + m_pose.getX() + ", Y:" + m_pose.getY() + ", Rot:" + m_pose.getRotation().getDegrees());
   }
-
+//TODO: bearing should be to or from *what*? split out?
   public double getBearing(){
     return filteredBearing;
   }
@@ -383,62 +371,6 @@ public class SwerveDrivetrain extends SubsystemBase {
     System.out.println("***BRAKES RELEASED***");
   }
 
-  public static PathPlannerTrajectory getPPPath(String pathName) {
-    return PathPlanner.loadPath(pathName, 1, 1); //last two parameters are max velocity and max accelleration
-  }
-
-  public static Command pathFactoryAuto(SwerveDrivetrain dt, Sensors_Subsystem sensors, double maxVel, double maxAcc, String pathname, int pathNum){
-    SwerveDrivetrain m_robotDrive = dt;
-    Sensors_Subsystem m_sensors = sensors;
-
-    var path = PathPlanner.loadPath(pathname, maxVel, maxAcc); //last two parameters are max velocity and max accelleration
-
-    if (path == null) {
-      return new InstantCommand();  // no path selected
-    }
-    
-    dt.m_field.getObject("Path" + pathNum).setTrajectory(path);
-
-    // get initial state from the trajectory
-    PathPlannerState initialState = path.getInitialState();
-    Pose2d startingPose = new Pose2d(initialState.poseMeters.getTranslation(), initialState.holonomicRotation);
-
-    PIDController xController = new PIDController(4.0, 0.0, 0.0, Constants.DT);   // [m]
-    PIDController yController = new PIDController(4.0, 0.0, 0.0, Constants.DT);   // [m]
-    PIDController thetaController = new PIDController(4, 0, 0, Constants.DT);     // [rad]
-      //Units are radians for thetaController; PPSwerveController is using radians internally.
-      thetaController.enableContinuousInput(-Math.PI, Math.PI); //prevent piroutte paths over continuity
-
-      PPSwerveControllerCommand swerveControllerCommand =
-      new PPSwerveControllerCommand(
-          path,
-          m_robotDrive::getPose, // Functional interface to feed supplier
-          m_robotDrive.getKinematics(),
-          // Position controllers 
-          xController,
-          yController,
-          thetaController,
-          m_robotDrive::drive,
-          m_robotDrive
-      );
-
-
-    // Run path following command, then stop at the end.
-    return new SequentialCommandGroup(
-      new InstantCommand(()-> {
-        m_robotDrive.setPose(startingPose);
-        if (pathNum == 1) m_sensors.setAutoStartPose(startingPose);
-      }),
-      new PrintCommand("***Factory: Running Path " + pathname),
-      swerveControllerCommand,
-      new InstantCommand(m_robotDrive::stop),
-      new PrintCommand("***Done Running Path " + pathname)
-      );
-  }
-
-  public PathPlannerTrajectory pathFactoryTele(Pose2d finalPoint) {
-    return PathPlanner.generatePath(new PathConstraints(1, 1), new PathPoint(m_pose.getTranslation(), finalPoint.getRotation(), m_pose.getRotation()), new PathPoint(finalPoint.getTranslation(), finalPoint.getRotation(), finalPoint.getRotation()));
-  }
 
   //  TODO: Move to a TEST/Tuning command  - DPL 2/21/22
   // private void pidTuning() { //if drivetrain tuning
