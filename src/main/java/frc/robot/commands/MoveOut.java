76c9d6efdc4e6701d408d35cf8535bc70f79c4fb
DerekL <derek@adminnet.us>
2023-01-21 15:48:11 -0600

    feedback/comments from Mr.L

--- src/main/java/frc/robot/commands/MoveOut.java
+++ src/main/java/frc/robot/commands/MoveOut.java
@@ -3,7 +3,29 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.ArmGeometry;
+import frc.robot.subsystems.Arm;
+
+/*
+ * comments form Mr.L
+ * 
+ * Dont think in terms of MoveOut, MoveIn, one command should handle both.
+ * 
+ * Think in terms of moveTo.  Arm is at X,Y,Z and we want it at X2, Y2,Z2.
+ * 
+ * It's hard to start commands without a sub-sys API, focus on that.
+ * 
+ * I've added a few hints and cleanups/.
+ * 
+ */
+
+
+
+ /*
+  TODO Add description of what/how this command should work,whats your intent?
+  
+  */
 
 public class MoveOut extends CommandBase {
   /** Creates a new MoveOut. */
@@ -14,20 +36,31 @@ public class MoveOut extends CommandBase {
   double[] lengths;
   double desiredArmLength;
 
-//instantiate object and call it
-  public  
+  
+    //State Vars
+  
+  //instantiate object and call it
 
-  public MoveOut(Arm arm) {
+  /**
+   * XYZ are blah blah
+   * 
+   * @param arm      required sub-system
+   * @param cmdX     target positions [tbd units]
+   * @param cmdY
+   * @param cmdZ
+   */
+  public MoveOut(Arm arm, double cmdX, double cmdY, double cmdZ) {
+
     // Use addRequirements() here to declare subsystem dependencies.
     //Assuming z is the final position
-    double z = ArmGeometry.getZLength();
-    this.arm = arm;
+    double z = ArmGeometry.geoZ;   //example getting geometry from constanst
+    this.arm = arm;      // you can use RobotContainer.RC().arm to avoid arm in signature
 
-
-    else {
-
-    }
+    addRequirements(arm);
   }
+
+
+
   // Setting Z to desired value
   public void setArmPos(double z){
     arm.setPos(z);
@@ -50,6 +83,9 @@ public class MoveOut extends CommandBase {
     if(interrupted){
       //Set arm to retracted position, guessing variable name
       //TODO put setRetractedPosition to variable in subsystems
+
+      //MrL - unlike would want to set another position, more likely stop motors or 
+      // just keep running so control loops hold the position.
       arm.setRetractedPosition;
     }
   }
@@ -58,6 +94,7 @@ public class MoveOut extends CommandBase {
   @Override
   public boolean isFinished() {
     //End once arm pos gets to desired pos
+    //MrL - 
     if(arm.getPos = arm.setPos){
       return true;
     }
