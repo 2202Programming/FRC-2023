84278a1cd6afe40e5bd325507831af0a73f5710e
Noah Ren <nren2025@gmail.com>
2023-01-23 21:05:18 -0600

    Sequential Commands for Claw WIP

---
+++ src/main/java/frc/robot/commands/EndAffector/OpenCloseClaw.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.EndAffector;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class OpenCloseClaw extends CommandBase {
+  final Claw claw;
+  final String name;
+  final boolean openClaw;
+  final boolean closeClaw;
+  /** Creates a new OpenClaw. */
+  public OpenCloseClaw(Claw claw, String name, boolean openClaw) {
+    this.claw = claw;
+    this.name = name;
+    this.openClaw = openClaw;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+  //Don't know if this can be done in the same class/file or if it needs separate class/files
+  public OpenCloseClaw(Claw claw, String name, boolean closeClaw){
+    this.claw = claw;
+    this.name = name;
+    this.closeClaw = closeClaw;
+    return closeClaw;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    claw.setClawPos(openClaw);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
