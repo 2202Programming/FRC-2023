ca7be8dcdc57d91b23a17147ce2aa7d23f642fe2
Noah Ren <nren2025@gmail.com>
2023-01-25 20:39:36 -0600

    Added command classes

--- src/main/java/frc/robot/commands/EndAffector/OpenCloseClaw.java
+++ src/main/java/frc/robot/commands/EndAffector/OpenCloseClaw.java
@@ -2,9 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
 package frc.robot.commands.EndAffector;
 
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Claw;
 
 public class OpenCloseClaw extends CommandBase {
   final Claw claw;
@@ -15,24 +18,30 @@ public class OpenCloseClaw extends CommandBase {
     this.name = name;
     this.clawWidth = clawWidth;
 
+
     // Use addRequirements() here to declare subsystem dependencies.
   }
   //Don't know if this can be done in the same class/file or if it needs separate class/files
 
 
-  // Called when the command is initially scheduled.
+
+
+  //TODO this (initialize)
   @Override
   public void initialize() {
-    claw.setClawPos(openClaw);
+
   }
+
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {}
 
+
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
+
 
   // Returns true when the command should end.
   @Override
--- src/main/java/frc/robot/commands/EndAffector/PlaceGamePiece.java
+++ src/main/java/frc/robot/commands/EndAffector/PlaceGamePiece.java
@@ -1,18 +1,26 @@
+
 //TODO Figure out how to set needToOpenClaw & needToCloseClaw, or how it will be determined (Probably calling a method)
 /**The goal is relatively simple, essentially establish whether the claw needs to be open or closed and do that
  * along with changing the claw to a certain angle if needed
 */
 package frc.robot.commands.EndAffector;
 
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.EndAffector.GamePieceAngle;
+import frc.robot.subsystems.Claw;
+
 
 
 public class PlaceGamePiece extends SequentialCommandGroup {
-  
+ 
 
-  /** Creates a new PlaceGamePiece. */
+
+  private Claw claw;
+
+
+/** Creates a new PlaceGamePiece. */
   public PlaceGamePiece(Claw claw) {
     super();
     //Don't know class name, claw for now, for the claw angle to place
@@ -23,30 +31,37 @@ public class PlaceGamePiece extends SequentialCommandGroup {
       new GamePieceAngle(claw, "grab", 25.0),
       new OpenCloseClaw(claw, "closedclaw", 0.0),
       new OpenCloseClaw(claw, "cubepick-up", 2.5),
-      new OpenCloseClaw(claw, "conepick-up", 2.0) 
+      new OpenCloseClaw(claw, "conepick-up", 2.0)
     );
     this.claw = claw;
 
+
   }
+
 
   public void operateClaw(){
     //Change angle of claw to what's needed
     //TODO change currAngle/adjustAngle to actual variable names when known
+
 
   }  
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled. 
+
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {}
+
 
   //If interrupted, open the claw
   @Override
   public void end(boolean interrupted) {
 
+
   }
+
 
   //Assuming default is open, if claw doesn't need to be closed, close the claw
   @Override
@@ -55,3 +70,7 @@ public class PlaceGamePiece extends SequentialCommandGroup {
     return super.isFinished();
   }
 }
+
+
+
+
