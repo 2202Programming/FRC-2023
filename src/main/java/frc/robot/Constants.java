6d68108301c6d2a816d5e0d65b494332041350c9
Nathanael Ren <nren2013@gmail.com>
2023-02-12 23:22:17 -0600

    first hack at synchronization

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -173,6 +173,8 @@ public final class Constants {
 
   public static final class ArmSettings {
 
+    public static final double MAX_VELOCITY = 1.0; // TODO: update me
+
   }
 
   // Intake Constants
