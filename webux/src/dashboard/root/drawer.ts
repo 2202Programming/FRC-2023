b3a55c372aa0ca7243bff356707e163a9770cd15
Ben Grogan <26grogab@elmbrookstudents.org>
2023-02-13 20:46:02 -0600

    Version4 UI
    Working with the author to get this worked out. Making lots of progress! Here's the link to the issue:
    https://github.com/frc-web-components/frc-web-components/issues/131

---
+++ webux/src/dashboard/root/drawer.ts
@@ -0,0 +1,200 @@
+/* eslint-disable import/extensions */
+import { LitElement, html, css, TemplateResult } from 'lit';
+import { customElement, state, property } from 'lit/decorators.js';
+import FrcDashboard from '../frc-dashboard';
+import './drawer-sidebar';
+
+@customElement('dashboard-drawer')
+export default class DashboardDrawer extends LitElement {
+  @property({ type: Object, attribute: false }) dashboard!: FrcDashboard;
+  @state() selectedElement?: HTMLElement;
+  @state() editors: HTMLElement[] = [];
+  @state() editorOpened: Record<string, boolean> = {};
+
+  static styles = css`
+    :host {
+      display: block;
+    }
+
+    .dashboard {
+      display: flex;
+    }
+
+    .editors {
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+      width: 370px;
+      gap: 10px;
+      font-family: sans-serif;
+      padding: 10px 0;
+      background: rgb(240, 240, 240);
+      box-sizing: border-box;
+    }
+
+    .editors.uneditable {
+      display: none;
+    }
+
+    details summary + * {
+      padding: 0 5px;
+      box-sizing: border-box;
+    }
+
+    details .opened-cursor {
+      display: none;
+      width: 15px;
+      margin-right: 3px;
+    }
+
+    details .closed-cursor {
+      display: inline-block;
+      width: 15px;
+      margin-right: 3px;
+    }
+
+    .editors summary {
+      font-weight: bold;
+      list-style: none;
+    }
+
+    details[open] > summary .opened-cursor {
+      display: inline-block;
+    }
+
+    details[open] > summary .closed-cursor {
+      display: none;
+    }
+
+    .editors-header {
+      font-size: 18px;
+      color: purple;
+      margin: 5px 0 10px;
+      padding: 0 10px;
+    }
+
+    .editor-components {
+      padding: 0 10px;
+      flex: 1;
+      overflow: auto;
+    }
+
+    .editor-components details {
+      margin-bottom: 15px;
+    }
+
+    details > summary {
+      list-style: none;
+    }
+
+    details > summary::marker,
+    details > summary::-webkit-details-marker {
+      display: none;
+    }
+  `;
+
+  firstUpdated(): void {
+    this.dashboard.subscribe('elementSelect', () => {
+      this.selectedElement = this.dashboard.getSelectedElement() ?? undefined;
+    });
+    this.selectedElement = this.dashboard.getSelectedElement() ?? undefined;
+  }
+
+  updated(updatedProps: Map<string, unknown>): void {
+    if (updatedProps.has('selectedElement')) {
+      this.#updateEditors();
+    }
+  }
+
+  #getEditorTags(): string[] {
+    return this.dashboard.getComponentIdsOfType('elementEditor');
+  }
+
+  #updateEditors(): void {
+    this.editors.forEach((editor) => {
+      editor.remove();
+      this.dashboard.unmount(editor);
+    });
+    const tags = this.#getEditorTags();
+    const editorComponents =
+      this.renderRoot.querySelector('.editor-components');
+    tags.forEach((tag) => {
+      const isOpened = this.editorOpened[tag] ?? true;
+      const editor = this.dashboard.create('elementEditor', tag);
+      if (editor) {
+        const container = document.createElement('details');
+        container.addEventListener('toggle', (ev) => {
+          const { open } = ev?.target as HTMLDetailsElement;
+          this.editorOpened = {
+            ...this.editorOpened,
+            [tag]: open,
+          };
+        });
+        if (isOpened) {
+          container.setAttribute('open', '');
+        }
+        container.innerHTML = `
+          <summary>
+            <span class="caret">
+              <vaadin-icon icon="vaadin:angle-right" class="closed-cursor"></vaadin-icon>
+              <vaadin-icon icon="vaadin:angle-down" class="opened-cursor"></vaadin-icon>
+            </span>
+            ${tag}
+          </summary>`;
+        container.appendChild(editor);
+        editorComponents?.append(container);
+        this.editors.push(container);
+      }
+    });
+  }
+
+  render(): TemplateResult {
+    const isEditable = this.dashboard.isElementEditable();
+    return html`
+      <div class="dashboard">
+        <dashboard-drawer-sidebar
+          .dashboard=${this.dashboard}
+        ></dashboard-drawer-sidebar>
+        <div class="editors ${!isEditable ? 'uneditable' : ''}">
+          ${this.selectedElement
+            ? html`
+                <header class="editors-header">
+                  ${this.dashboard?.getElementDisplayName(this.selectedElement)}
+                </header>
+              `
+            : null}
+          <div class="editor-components">${this.renderElementTree()}</div>
+        </div>
+      </div>
+    `;
+  }
+
+  renderElementTree(): TemplateResult {
+    if (!this.selectedElement) {
+      return html``;
+    }
+    const selectedTab = this.selectedElement.closest('dashboard-tab');
+    return html`
+      <details open>
+        <summary>
+          <span class="caret">
+            <vaadin-icon
+              icon="vaadin:angle-right"
+              class="closed-cursor"
+            ></vaadin-icon>
+            <vaadin-icon
+              icon="vaadin:angle-down"
+              class="opened-cursor"
+            ></vaadin-icon>
+          </span>
+          Element Tree
+        </summary>
+        <dashboard-element-tree-node
+          .element=${selectedTab}
+          .dashboard=${this.dashboard}
+          expanded
+        ></dashboard-element-tree-node>
+      </details>
+    `;
+  }
+}
---
+++ webux/src/dashboard/root/index.ts
@@ -0,0 +1,235 @@
+/* eslint-disable import/extensions */
+import { LitElement, html, css, TemplateResult, render } from 'lit';
+import './dashboard-tab';
+import './navbar';
+import './drawer';
+import { customElement, property, state } from 'lit/decorators.js';
+import { guard } from 'lit/directives/guard.js';
+import { onRemoveKeyPress } from '../hotkeys';
+import FrcDashboard from '../frc-dashboard';
+import './source-picker-dialog';
+import removeElement from './remove-element';
+
+const styles = css`
+  :host {
+    display: flex;
+    flex-direction: row;
+    font-family: sans-serif;
+    font-size: 16px;
+    width: 100vw;
+    overflow: hidden;
+  }
+
+  .layout {
+    width: 100%;
+    box-sizing: border-box;
+    display: flex;
+  }
+
+  .layout.closed dashboard-drawer {
+    display: none;
+  }
+
+  .dashboard {
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
+    flex: 1;
+    height: 100vh;
+  }
+
+  .dashboard-elements {
+    width: 100%;
+    overflow: auto;
+    flex: 1;
+  }
+
+  #container {
+    position: relative;
+    height: 100%;
+    width: 100vw;
+    box-sizing: border-box;
+  }
+
+  ::slotted([slot='dashboard']) {
+    width: 100%;
+    height: 100%;
+    background: var(--dashboard-background, white);
+  }
+
+  ::slotted([slot='layer']) {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    box-sizing: border-box;
+    pointer-events: none;
+  }
+`;
+
+@customElement('dashboard-root')
+export default class DashboardRoot extends LitElement {
+  @state() drawerOpened = true;
+  @state() ready = false;
+  @state() dialogOpened = false;
+  @property({ type: Object, attribute: false }) dashboard?: FrcDashboard;
+
+  static styles = styles;
+
+  constructor() {
+    super();
+    this.drawerOpened = true;
+    document.body.style.margin = '0';
+  }
+
+  get #selectedElement(): HTMLElement | null {
+    return this.dashboard?.getSelectedElement() ?? null;
+  }
+
+  async firstUpdated(): Promise<void> {
+    if (!this.dashboard) {
+      return;
+    }
+
+    this.dashboard.addElements({
+      'dashboard-tab': {
+        dashboard: {
+          topLevel: false,
+          displayName: (element) => element.getAttribute('tab-name') || 'Tab',
+          layout: {
+            type: 'absolute',
+            movable: false,
+            resizable: {
+              horizontal: false,
+              vertical: false,
+            },
+          },
+        },
+        properties: {
+          tabName: { type: 'String', attribute: 'tab-name', reflect: true },
+        },
+        slots: [{ name: '' }],
+      },
+    });
+    const rootElement = this.dashboard.getRootElement();
+    rootElement.setAttribute('slot', 'dashboard');
+    this.append(rootElement);
+    this.dashboard.resetHtml();
+
+    onRemoveKeyPress(() => {
+      if (this.#selectedElement) {
+        if (this.dashboard) {
+          if (this.#selectedElement.tagName !== 'DASHBOARD-TAB') {
+            const nextElement = removeElement(
+              this.#selectedElement,
+              this.dashboard.getConnector()
+            );
+            if (nextElement) {
+              this.dashboard.setSelectedElement(nextElement);
+            }
+          }
+        }
+      }
+    });
+
+    Object.entries(this.dashboard.getLayers()).forEach(([_, layerElement]) => {
+      this.appendChild(layerElement);
+    });
+
+    this.dashboard.subscribe('layerAdd', (value: any) => {
+      this.appendChild(value.layer);
+    });
+
+    const navbar = document.createElement('dashboard-navbar');
+    (navbar as any).dashboard = this.dashboard;
+    navbar.setAttribute('slot', 'navbar');
+    navbar.addEventListener('drawerToggle', () => {
+      this.#onDrawerToggle();
+    });
+    this.appendChild(navbar);
+
+    this.dashboard.subscribe('themeSet', () => this.#updateTheme());
+    this.#updateTheme();
+
+    this.dashboard.subscribe('sourcesDialogOpen', () => {
+      this.dialogOpened = true;
+    });
+
+    this.ready = true;
+  }
+
+  #updateTheme(): void {
+    const navbar = this.querySelector('dashboard-navbar');
+    const theme = this.dashboard?.getTheme() ?? '';
+    navbar?.setAttribute('data-theme', theme);
+  }
+
+  updated(updatedProps: Map<string, unknown>): void {
+    if (updatedProps.has('drawerOpened')) {
+      if (this.drawerOpened) {
+        this.dashboard?.openDrawer();
+      } else {
+        this.dashboard?.closeDrawer();
+      }
+    }
+  }
+
+  #onDrawerToggle(): void {
+    this.drawerOpened = !this.drawerOpened;
+    this.dashboard?.setPreviewedElement(null);
+    const layout = this.renderRoot.querySelector('.layout') as HTMLElement;
+    const drawer = this.renderRoot.querySelector(
+      'dashboard-drawer'
+    ) as HTMLElement;
+    if (this.drawerOpened && layout && drawer) {
+      layout.classList.remove('closed');
+    } else {
+      layout.classList.add('closed');
+    }
+  }
+
+  render(): TemplateResult {
+    if (!this.ready) {
+      return html``;
+    }
+    const isEditable = this.dashboard?.isElementEditable();
+    return html`
+      <div class="layout">
+        <vaadin-dialog
+          theme="no-padding"
+          draggable
+          modeless
+          resizable
+          .opened=${this.drawerOpened && this.dialogOpened}
+          .renderer=${guard([], () => (root: HTMLElement) => {
+            render(
+              html`
+                <dashboard-source-picker-dialog
+                  .dashboard=${this.dashboard}
+                  .dialogOpened=${this.dialogOpened}
+                  @closeDialog=${() => {
+                    this.dialogOpened = false;
+                  }}
+                ></dashboard-source-picker-dialog>
+              `,
+              root
+            );
+          })}
+        ></vaadin-dialog>
+        <dashboard-drawer
+          .interact="${null}"
+          .dashboard=${this.dashboard}
+        ></dashboard-drawer>
+        <div class="dashboard">
+          <slot name="navbar"></slot>
+          <div class="dashboard-elements">
+            <div id="container">
+              <slot name="dashboard"></slot>
+              ${isEditable ? html` <slot name="layer"></slot> ` : ''}
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
