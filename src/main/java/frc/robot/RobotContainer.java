55ac931c0880d26a075daa9cee9cdaa5ebce3dd3
Beast Software <beastsoftwaredev@gmail.com>
2023-02-13 19:06:01 -0600

    debuging

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -48,10 +48,10 @@ public class RobotContainer {
     return rc;
   }
 
-  //control binding mode
-  enum Bindings{
-    Competition,    
-    //below are testing modes, add as needed
+  // control binding mode
+  enum Bindings {
+    Competition,
+    // below are testing modes, add as needed
     vision_test,
     balance_test,
     arm_test,
@@ -71,9 +71,10 @@ public class RobotContainer {
 
   // vision systems, create on every bot
   public final PhotonVision photonVision = new PhotonVision();
-  public final Limelight_Subsystem limelight = new Limelight_Subsystem();;
+  // public final Limelight_Subsystem limelight = new Limelight_Subsystem();;
   public HashMap<String, Command> eventMap;
   public SwerveAutoBuilder autoBuilder;
+
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
@@ -81,8 +82,7 @@ public class RobotContainer {
     RobotContainer.rc = this; // for singleton accesor
     robotSpecs = new RobotSpecs(); // mechanism to pull different specs based on roborio SN
     dc = new HID_Xbox_Subsystem(0.3, 0.9, 0.05); // TODO: deal with driver prefs
-    initEvents();  //setup event hashmap
-    limelight.setPipeline(0);
+    //limelight.setPipeline(0);
     // Construct sub-systems based on robot Name Specs
     switch (robotSpecs.myRobotName) {
       case CompetitionBot2023:
@@ -94,6 +94,14 @@ public class RobotContainer {
         break;
 
       case SwerveBot:
+        sensors = new Sensors_Subsystem();
+        drivetrain = new SwerveDrivetrain();
+        intake = null;
+        armSS = null;
+        claw = null;
+        break;
+
+      case ChadBot:
         sensors = new Sensors_Subsystem();
         drivetrain = new SwerveDrivetrain();
         intake = null;
@@ -117,35 +125,40 @@ public class RobotContainer {
       drivetrain.setDefaultCommand(new FieldCentricDrive(drivetrain));
     }
 
+    initEvents(); // setup event hashmap
     // Edit the binding confiuration for testing
     configureBindings(Bindings.Competition);
 
     autoBuilder = new SwerveAutoBuilder(
-      drivetrain::getPose, // Pose2d supplier
-      drivetrain::resetPose, // Pose2d consumer, used to reset odometry at the beginning of auto
-      drivetrain.getKinematics(), // SwerveDriveKinematics
-      new PIDConstants(4.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
-      new PIDConstants(2.0, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
-      drivetrain::drive, // Module states consumer used to output to the drive subsystem
-      RobotContainer.RC().eventMap,
-      true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-      drivetrain // The drive subsystem. Used to properly set the requirements of path following commands
+        drivetrain::getPose, // Pose2d supplier
+        drivetrain::resetPose, // Pose2d consumer, used to reset odometry at the beginning of auto
+        drivetrain.getKinematics(), // SwerveDriveKinematics
+        new PIDConstants(4.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y
+                                         // PID controllers)
+        new PIDConstants(2.0, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation
+                                         // controller)
+        drivetrain::drive, // Module states consumer used to output to the drive subsystem
+        RobotContainer.RC().eventMap,
+        true, // Should the path be automatically mirrored depending on alliance color.
+              // Optional, defaults to true
+        drivetrain // The drive subsystem. Used to properly set the requirements of path following
+                   // commands
     );
 
   }
 
-
   private void configureBindings(Bindings bindings) {
     // bindings useful for everyone
-    
+
     // Y button to reset current facing to zero
     if (drivetrain != null) {
       dc.Driver().y().whileTrue(new InstantCommand(() -> {
-        drivetrain.resetAnglePose(new Rotation2d(0));    }));
+        drivetrain.resetAnglePose(new Rotation2d(0));
+      }));
     }
 
     // add bindings based on current user mode
-    switch (bindings){
+    switch (bindings) {
       case arm_test:
         break;
       case balance_test:
@@ -157,18 +170,18 @@ public class RobotContainer {
 
       case vision_test:
         // X button to change LL pipeline
-        dc.Driver().leftBumper().onTrue(new InstantCommand(() -> {
-          limelight.togglePipeline();
-        }));
+        // dc.Driver().leftBumper().onTrue(new InstantCommand(() -> {
+        //   limelight.togglePipeline();
+        // }));
         dc.Driver().a().whileTrue(new CenterTapeYaw());
         dc.Driver().b().whileTrue(new CenterTapeSkew());
-        dc.Driver().x().whileTrue(new CenterTapeYawSkew());      
+        dc.Driver().x().whileTrue(new CenterTapeYawSkew());
         break;
 
       case Competition:
       default:
-        //TODO: Put Competition bindings here
-        break;      
+        // TODO: Put Competition bindings here
+        break;
     }
   }
 
@@ -181,11 +194,15 @@ public class RobotContainer {
     return new autoCommand();
   }
 
- public void initEvents(){
+  public void initEvents() {
     eventMap = new HashMap<>();
-    eventMap.put("start", new SequentialCommandGroup(new PrintCommand("***Path Start"), new InstantCommand(drivetrain::printPose)));
-    eventMap.put("middle", new SequentialCommandGroup(new PrintCommand("***Path Middle"), new InstantCommand(drivetrain::printPose)));
-    eventMap.put("end", new SequentialCommandGroup(new PrintCommand("***Path End"), new InstantCommand(drivetrain::printPose), new ChargeStationBalance(true)));
-    eventMap.put("score", new SequentialCommandGroup(new PrintCommand("***Path score"), new InstantCommand(drivetrain::printPose), new WaitCommand(2)));
+    eventMap.put("start",
+        new SequentialCommandGroup(new PrintCommand("***Path Start"), new InstantCommand(drivetrain::printPose)));
+    eventMap.put("middle",
+        new SequentialCommandGroup(new PrintCommand("***Path Middle"), new InstantCommand(drivetrain::printPose)));
+    eventMap.put("end", new SequentialCommandGroup(new PrintCommand("***Path End"),
+        new InstantCommand(drivetrain::printPose), new ChargeStationBalance(true)));
+    eventMap.put("score", new SequentialCommandGroup(new PrintCommand("***Path score"),
+        new InstantCommand(drivetrain::printPose), new WaitCommand(2)));
   }
 }
