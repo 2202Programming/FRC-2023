fdd11e4214d144b824c50543f132f4bc3851ec66
Ben Grogan <26grogab@elmbrookstudents.org>
2023-02-13 20:46:02 -0600

    Version4 UI
    Working with the author to get this worked out. Making lots of progress! Here's the link to the issue:
    https://github.com/frc-web-components/frc-web-components/issues/131

---
+++ webux/src/dashboard/dashboard-components/element-editors/properties-editor/index.js
@@ -0,0 +1,184 @@
+import { LitElement, html, css } from 'lit';
+import './dashboard-component-render';
+
+const styles = css`
+  :host {
+    display: block;
+    font-family: sans-serif;
+    height: 100%;
+    box-sizing: border-box;
+  }
+
+  p {
+    margin-top: 0;
+    font-weight: bold;
+  }
+
+  p span {
+    color: purple;
+  }
+
+  .properties-view {
+    padding-bottom: 10px;
+  }
+
+  .properties-view:last-child {
+    padding-bottom: 0;
+  }
+`;
+
+class PropertiesEditor extends LitElement {
+  static properties = {
+    dashboard: { attribute: false },
+    dialogOpened: { state: true },
+  };
+
+  static styles = styles;
+
+  constructor() {
+    super();
+    this.disalogOpened = false;
+    this.sourceChangeObserver = null;
+  }
+
+  get #element() {
+    return this.dashboard.getSelectedElement();
+  }
+
+  get #connector() {
+    return this.dashboard.getConnector();
+  }
+
+  get inputElement() {
+    return this.renderRoot.querySelector('[part=input]');
+  }
+
+  get webbit() {
+    return this.#element && this.#connector?.getElementWebbit(this.#element);
+  }
+
+  get sourceKey() {
+    return this.webbit?.sourceKey ?? '';
+  }
+
+  get sourceProvider() {
+    return this.webbit?.sourceProvider ?? '';
+  }
+
+  get store() {
+    return this.dashboard.getStore();
+  }
+
+  get defaultSourceProvider() {
+    return this.store?.getDefaultSourceProvider();
+  }
+
+  get sourceDialog() {
+    return this.renderRoot.querySelector('vaadin-dialog');
+  }
+
+  updateSourceChangedObserver() {
+    this.sourceChangeObserver?.disconnect();
+    if (this.#element) {
+      this.sourceChangeObserver = new MutationObserver(() => {
+        this.dashboard.publish('sourceChange', {
+          sourceKey: this.sourceKey,
+          sourceProvider: this.sourceProvider,
+          element: this.#element,
+        });
+        this.requestUpdate();
+      });
+      this.sourceChangeObserver.observe(this.#element, {
+        attributes: true,
+        attributeFilter: ['source-provider', 'source-key'],
+      });
+    }
+  }
+
+  firstUpdated() {
+    this.dashboard.subscribe('elementSelect', () => {
+      this.requestUpdate();
+    });
+    this.updateSourceChangedObserver();
+  }
+
+  disconnectedCallback() {
+    this.sourceChangeObserver?.disconnect();
+  }
+
+  render() {
+    if (!this.#element) {
+      return html``;
+    }
+
+    const webbit = this.#connector?.getElementWebbit(this.#element);
+
+    if (!webbit) {
+      return html``;
+    }
+    const { properties } = webbit.getConfig();
+
+    return html`
+      <vaadin-text-field
+        part="source-key-dropdown"
+        label=${'Source' +
+        (this.sourceProvider ? ` (${this.sourceProvider})` : '') +
+        ':'}
+        theme="small"
+        readonly
+        .value=${this.sourceKey || 'Connect to a data source...'}
+        style="width: 100%; margin-bottom: 10px"
+      >
+        <vaadin-icon
+          slot="suffix"
+          icon="vaadin:edit"
+          style="cursor: pointer"
+          title="edit"
+          @click=${() => this.dashboard.publish('sourcesDialogOpen')}
+        ></vaadin-icon>
+      </vaadin-text-field>
+      <div class="properties-view">
+        <vaadin-form-layout @change=${this.onValueChange}>
+          ${Object.entries(properties)
+            .filter(
+              ([, { type }]) => !['SourceProvider', 'Store'].includes(type)
+            )
+            .filter(([, { input }]) => input?.type !== 'None')
+            .map(([name]) => {
+              return this.renderPropertyView(
+                name,
+                webbit.getPropertyHandler(name)
+              );
+            })}
+        </vaadin-form-layout>
+      </div>
+    `;
+  }
+
+  renderPropertyView(name, propertyHandler) {
+    const property = propertyHandler.getProperty();
+
+    const inputType = property.input?.type ?? property.type;
+
+    return html`
+      <dashboard-component-renderer
+        @change=${(event) => {
+          this.dashboard.publish('propertyChange', {
+            ...event.detail,
+            element: this.#element,
+          });
+        }}
+        component-type="propertyInput"
+        component-id=${inputType}
+        .config=${{
+          element: this.#element,
+          propertyHandler,
+          propertyName: name,
+        }}
+        .dashboard=${this.dashboard}
+      ></dashboard-component-renderer>
+    `;
+  }
+}
+
+customElements.define('dashboard-properties-editor', PropertiesEditor);
---
+++ webux/src/dashboard/root/drawer-sidebar.ts
@@ -0,0 +1,286 @@
+/* eslint-disable import/extensions */
+import { LitElement, html, css, TemplateResult } from 'lit';
+import { customElement, state, property } from 'lit/decorators.js';
+import { WebbitConfig } from '@webbitjs/webbit';
+import FrcDashboard from '../frc-dashboard';
+import { appendElementToDashboard } from './get-element-html';
+
+interface DashboardElement {
+  selector: string;
+  name: string;
+}
+
+@customElement('dashboard-drawer-sidebar')
+export default class DashboardDrawerSidebar extends LitElement {
+  @property({ type: Object }) dashboard!: FrcDashboard;
+  @state() groups: string[] = [];
+  @state() newElementSelector?: string;
+  @state() selectedGroup = 'My Elements';
+  @state() selectedElement?: HTMLElement;
+
+  static styles = css`
+    :host {
+      font-family: sans-serif;
+      font-size: 15px;
+      width: 200px;
+      height: 100vh;
+      background: #444;
+      padding: 20px 15px;
+      display: flex;
+      flex-direction: column;
+      overflow: auto;
+      box-sizing: border-box;
+      color: white;
+    }
+
+    header {
+      text-transform: uppercase;
+      margin-bottom: 10px;
+      font-size: 18px;
+      color: lightblue;
+    }
+
+    p {
+      margin: 0 0 5px;
+      cursor: grab;
+      line-height: 18px;
+    }
+
+    p:hover {
+      font-weight: bold;
+    }
+
+    p.selected {
+      font-weight: bold;
+    }
+
+    .group-selector {
+      margin-bottom: 10px;
+      padding: 3px 1px;
+      font-size: 16px;
+    }
+
+    .add-button {
+      border: 1px solid #aaa;
+      color: white;
+      background: none;
+      border-radius: 4px;
+      padding: 3px 5px;
+      cursor: pointer;
+    }
+
+    .demo-button {
+      color: rgb(187, 187, 255);
+      border: none;
+      background: none;
+      cursor: pointer;
+      text-align: left;
+      margin-bottom: 5px;
+      padding: 1px;
+    }
+
+    .no-children-warning span {
+      font-weight: bold;
+    }
+  `;
+
+  get #allowedChildren(): string[] {
+    const allowedChildren =
+      this.dashboard?.getAllowedChildren()?.[0]?.allowedChildren;
+    return allowedChildren ?? [];
+  }
+
+  #appendToDashboard(): void {
+    if (this.selectedElement && this.newElementSelector) {
+      appendElementToDashboard(
+        this.dashboard.getConnector(),
+        this.newElementSelector,
+        this.selectedElement
+      );
+    }
+  }
+
+  getGroups(): string[] {
+    const { dashboard } = this;
+    if (!dashboard) {
+      return [];
+    }
+    const selectors = this.#allowedChildren;
+    const groups: string[] = selectors.map((selector) => {
+      const config = dashboard.getConnector().getElementConfig(selector);
+      if (!config) {
+        return '';
+      }
+      return config.group;
+    });
+    return [...new Set(groups)].filter((group) => group !== '').sort();
+  }
+
+  firstUpdated(): void {
+    this.dashboard.subscribe('elementSelect', () => {
+      this.selectedElement = this.dashboard.getSelectedElement() ?? undefined;
+    });
+    this.dashboard.subscribe('elementsAdd', () => {
+      this.groups = this.getGroups();
+    });
+    this.selectedElement = this.dashboard.getSelectedElement() ?? undefined;
+  }
+
+  updated(changedProps: Map<string, unknown>): void {
+    if (changedProps.has('selectedElement')) {
+      this.groups = this.getGroups();
+      if (!this.groups.includes(this.selectedGroup)) {
+        this.selectedGroup = this.groups[0] ?? this.selectedGroup;
+      }
+      this.newElementSelector = this.getElements()[0]?.selector;
+    }
+    if (changedProps.has('selectedGroup')) {
+      const selectedElementGroup =
+        this.getElementConfig()?.group ?? this.groups[0];
+      if (selectedElementGroup !== this.selectedGroup) {
+        this.newElementSelector = this.getElements()[0]?.selector;
+      }
+    }
+  }
+
+  getElementConfig(): WebbitConfig | undefined {
+    return this.dashboard
+      ?.getConnector()
+      .getMatchingElementConfig(this.selectedElement);
+  }
+
+  getElements(): DashboardElement[] {
+    const { dashboard } = this;
+    if (!dashboard) {
+      return [];
+    }
+    const selectors = this.#allowedChildren;
+    return selectors
+      .filter((selector) => {
+        const config = dashboard.getConnector().getElementConfig(selector);
+        if (!config) {
+          return false;
+        }
+        return config.group === this.selectedGroup;
+      })
+      .map((selector) => ({
+        selector,
+        name: dashboard.getSelectorDisplayName(selector),
+      }))
+      .sort((el1, el2) => el1.name.localeCompare(el2.name));
+  }
+
+  #getSelectedElementName(): string {
+    const { selectedElement } = this;
+    if (!selectedElement) {
+      return '';
+    }
+    return this.dashboard?.getElementDisplayName(selectedElement) ?? '';
+  }
+
+  render(): TemplateResult {
+    const isEditable = this.dashboard.isElementEditable();
+    return html`
+      <header>Elements</header>
+      <select
+        class="group-selector"
+        ?disabled=${this.groups.length === 0 || !isEditable}
+        @change=${(ev: any) => {
+          this.selectedGroup = ev.target.value;
+        }}
+      >
+        ${this.groups.map(
+          (group) => html`
+            <option value=${group} ?selected=${group === this.selectedGroup}>
+              ${group}
+            </option>
+          `
+        )}
+      </select>
+      ${this.#renderChildren()}
+    `;
+  }
+
+  #renderChildren(): TemplateResult {
+    const isEditable = this.dashboard.isElementEditable();
+    if (!isEditable) {
+      return html`
+        <p class="no-children-warning">
+          No children can be added to a tutorial
+        </p>
+      `;
+    }
+    return html`
+      ${this.getElements().length === 0
+        ? html`
+            <p class="no-children-warning">
+              No children can be added to element
+              <span>${this.#getSelectedElementName()}</span>
+            </p>
+          `
+        : null}
+      ${this.getElements().map(
+        ({ selector, name }) => html`
+          <p
+            class="${this.newElementSelector === selector ? 'selected' : ''}"
+            key=${selector}
+            draggable="true"
+            @dragstart=${(event: DragEvent) => {
+              const img = document.createElement('img');
+              img.src =
+                'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+              event.dataTransfer?.setDragImage(img, 0, 0);
+
+              this.dashboard.publish('dragNewElementStart', {
+                selector,
+                event,
+              });
+            }}
+            @dragend=${(event: Event) =>
+              this.dashboard.publish('dragNewElementEnd', { event })}
+            @click=${() => {
+              this.newElementSelector = selector;
+            }}
+          >
+            ${name}
+          </p>
+          ${this.newElementSelector === selector
+            ? html`
+                ${this.renderDemo()}
+                <div style="margin-bottom: 8px">
+                  <button class="add-button" @click=${this.#appendToDashboard}>
+                    Prepend
+                  </button>
+                  <button class="add-button" @click=${this.#appendToDashboard}>
+                    Append
+                  </button>
+                </div>
+              `
+            : null}
+        `
+      )}
+    `;
+  }
+
+  renderDemo(): TemplateResult {
+    if (!this.newElementSelector) {
+      return html``;
+    }
+    const tutorials = this.dashboard.getElementTutorials(
+      this.newElementSelector
+    );
+    if (tutorials.length === 0) {
+      return html``;
+    }
+    return html`
+      <button
+        class="demo-button"
+        @click=${() => {
+          this.dashboard.showTutorial(tutorials[0].id);
+        }}
+      >
+        Demo element
+      </button>
+    `;
+  }
+}
