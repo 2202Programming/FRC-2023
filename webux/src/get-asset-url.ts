b330a29eeb1bcd0a2a7eb77aaa283c017e20792a
Ben Grogan <26grogab@elmbrookstudents.org>
2023-02-15 21:12:43 -0600

    Don't need UI code

--- webux/src/get-asset-url.ts
+++
@@ -1,4 +0,0 @@
-export default function getAssetUrl(relativePath: string): string {
-  const origin = import.meta.url ? new URL(import.meta.url).origin : '';
-  return `${origin}/assets/${relativePath}`;
-}
