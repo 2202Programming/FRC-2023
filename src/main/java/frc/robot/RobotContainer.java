87519d0056f1841e8b82b38655604fdb78931894
Beast Software <beastsoftwaredev@gmail.com>
2023-02-15 22:22:43 -0600

    more sparkmax woes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -134,7 +134,7 @@ public class RobotContainer {
     // add bindings based on current user mode
     switch (bindings){
       case arm_test:
-      dc.Driver().a().whileTrue(new MoveArmsTest(10.0));
+      dc.Driver().a().whileTrue(new MoveArmsTest(10.0, 2.0));
         break;
       case balance_test:
         dc.Driver().rightBumper().whileTrue(new ChargeStationBalance(false));
