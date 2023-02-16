f11240235d8919ee0333ac17ebc77c0e9f36bd40
Ben Grogan <26grogab@elmbrookstudents.org>
2023-02-15 21:12:43 -0600

    Don't need UI code

--- webux/src/elements/frc-elements/drivebases/index.js
+++
@@ -1,4 +0,0 @@
-import * as differential from './differential';
-import * as mecanum from './mecanum';
-
-export const drivebaseElements = [differential, mecanum];
--- webux/src/source-providers/index.ts
+++
@@ -1,9 +0,0 @@
-import { FrcDashboard } from '../dashboard';
-import GamepadProvider from './gamepad-provider';
-import Nt4Provider from './nt4/nt4-provider';
-
-export default function addProviders(dashboard: FrcDashboard): void {
-  dashboard.addSourceProvider('NetworkTables', new Nt4Provider());
-  dashboard.addSourceProvider('Gamepad', new GamepadProvider());
-  dashboard.setDefaultSourceProvider('NetworkTables');
-}
