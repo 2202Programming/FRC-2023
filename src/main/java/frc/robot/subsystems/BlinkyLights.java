9fc77d1bb180c0ecc4ac52e4039eef26fe1b6e65
nathanaelren <58539761+nathanaelren@users.noreply.github.com>
2023-02-18 12:57:35 -0600

    cmmts that are pushed

--- src/main/java/frc/robot/subsystems/BlinkyLights.java
+++ src/main/java/frc/robot/subsystems/BlinkyLights.java
@@ -18,10 +18,12 @@ public class BlinkyLights extends SubsystemBase {
   Color blue = new Color(0, 0, 255);
   Color black = new Color(0, 0, 0);
    
+// consider passing through a single Color parameter here --nren
 public void setColor(int r, int g, int b){
     candle.setLEDs(r, g, b);
 }
 
+// each of these should call the setColor(r/g/b) command --nren
   public void setOrange(){
     candle.setLEDs(265, 165, 0);
   }
@@ -35,6 +37,7 @@ public void setColor(int r, int g, int b){
   
   /** Creates a new BlinkyLights. */
   public BlinkyLights() {
+    // do something with the config --nren
    candle = new CANdle(0);
   }
 /*
@@ -42,6 +45,7 @@ public void setColor(int r, int g, int b){
 */
 
   public void setBrightness(double brightness) {
+    // directl set the candle's brightness, not the config's brightness --nren
     config.brightnessScalar = brightness;
   }
 
@@ -51,4 +55,6 @@ public void setColor(int r, int g, int b){
     // This method will be called once per scheduler run
 
   }
+
+  // give me some NTs --nren
 }
