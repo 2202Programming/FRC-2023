9b0d04f935cf02c34713663c579838c8d30e6aa6
nathanaelren <58539761+nathanaelren@users.noreply.github.com>
2023-02-18 12:30:14 -0600

    whoops fixed rc

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,7 @@ import frc.robot.commands.Automation.CenterTapeYaw;
 import frc.robot.commands.Automation.CenterTapeYawSkew;
 import frc.robot.commands.auto.autoCommand;
 import frc.robot.commands.swerve.ChargeStationBalance;
-import frc.robot.commands.swerve.ChargeStationBalanceChad;
+import frc.robot.commands.swerve.ChargeStationBalanceRoll; // default is pitch
 import frc.robot.commands.swerve.FieldCentricDrive;
 import frc.robot.commands.test.ArmMoveAtSpeed_L_R_test;
 import frc.robot.commands.test.ArmPositionTest;
@@ -188,7 +188,7 @@ public class RobotContainer {
         break;
       case balance_test:
         if (drivetrain == null) break;
-        dc.Driver().rightBumper().whileTrue(new ChargeStationBalanceChad(false));
+        dc.Driver().rightBumper().whileTrue(new ChargeStationBalance(false));
         break;
 
       case claw_test:
@@ -238,7 +238,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return new autoCommand().andThen(new ChargeStationBalanceChad());
+    return new autoCommand().andThen(new ChargeStationBalance());
   }
 
   /**
