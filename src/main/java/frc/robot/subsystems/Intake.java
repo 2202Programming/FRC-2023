c8173f090d74cd91446db7b20359a8af6fa3e746
nathanaelren <58539761+nathanaelren@users.noreply.github.com>
2023-02-18 12:06:26 -0600

    added car wash basic functionality

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,12 +10,20 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 import frc.robot.Constants.CAN;
 import frc.robot.Constants.PCM1;
 
+/*
+ * On the intake: upper and lower motor and pneumatics (double solenoid)
+ * On the car wash: left and right motor
+ * Everything is a NEO on a Spark Max
+ */
+
 public class Intake extends SubsystemBase {
 
-  final double IntakeMotorStrength = 0.6; //used for default ON
+  double IntakeMotorStrength = 0.6; //used for default ON
+  double CarwashMotorStrength = 0.5; // used for default ON TODO move to constatns (same with above)
 
   //Localized Constants - what valve value does what action
   static final Value DEPLOY  = Value.kReverse;
@@ -28,6 +36,8 @@ public class Intake extends SubsystemBase {
               PneumaticsModuleType.CTREPCM,
               PCM1.INTAKE_UP_SOLENOID_PCM,
               PCM1.INTAKE_DOWN_SOLENOID_PCM);
+  final CANSparkMax l_carwash_mtr = new CANSparkMax(CAN.CARWASH_LEFT_MTR, CANSparkMax.MotorType.kBrushless);
+  final CANSparkMax r_carwash_mtr = new CANSparkMax(CAN.CARWASH_RIGHT_MTR, CANSparkMax.MotorType.kBrushless);
 
   //TODO: dpl  1/27/23 possible lightgate for triggering - check with mechanical team
   
@@ -35,7 +45,10 @@ public class Intake extends SubsystemBase {
   public Intake() {
     motor_config(l_intake_mtr, false);
     motor_config(r_intake_mtr, true);    //TODO: Check if inversion is needed
-    
+    motor_config(l_carwash_mtr, false);
+    motor_config(r_carwash_mtr, false); // TODO: check if inversion is needed
+
+    ntconfig();
   }
 
   void motor_config(CANSparkMax mtr, boolean inverted) {
@@ -46,21 +59,21 @@ public class Intake extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    ntupdates();
   }
 
   //Turn Intake Motor On by sending a double value
-  public void on(double intakeMotorStrength) {
+  public void setIntakeSpeed(double intakeMotorStrength) {
     l_intake_mtr.set(intakeMotorStrength);
     r_intake_mtr.set(intakeMotorStrength);
   }
 
-  public void on(){    //on() with no-args is default
-    on(IntakeMotorStrength);
+  public void intakeOn(){    //on() with no-args is default
+    setIntakeSpeed(IntakeMotorStrength);
   }   
 
-  public void off() {
-    on(0);
+  public void intakeOff() {
+    setIntakeSpeed(0.0);
   }
 
   //Deploy arm mechanism using a Double Solenoids
@@ -76,6 +89,31 @@ public class Intake extends SubsystemBase {
   //Returns the state of the Intake Arm
   public static boolean isDeployed() {
     return (intake_solenoid.get() == DEPLOY); 
+  }
+
+  public void setCarwashSpeed(double carwashMotorStrength) {
+    l_carwash_mtr.set(carwashMotorStrength);
+    r_carwash_mtr.set(carwashMotorStrength);
+  }
+
+  public void carwashOn() {
+    setCarwashSpeed(CarwashMotorStrength);
+  }
+
+  public void carwashOff() {
+    setCarwashSpeed(0.0);
+  }
+
+  /**
+   * NTs
+   */
+
+  public void ntconfig() {
+
+  }
+
+  public void ntupdates() {
+
   }
 
 }
