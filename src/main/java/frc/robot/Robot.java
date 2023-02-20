5dc15cbc755a4865bf5b84b484a51213d0014712
Jason <jruben4@yahoo.com>
2023-02-19 22:22:24 -0600

    init rumble and match timer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.MatchTimer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -78,6 +79,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    new MatchTimer().schedule(); //a match time reporter for the telop portion of the match
   }
 
   /** This function is called periodically during operator control. */
