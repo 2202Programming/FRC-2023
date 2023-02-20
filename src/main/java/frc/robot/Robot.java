74ab0dd7f15179802ad71ebe1eca59e3df0cc7ae
Jason <jruben4@yahoo.com>
2023-02-19 22:22:24 -0600

    init rumble and match timer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.MatchTimer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -78,6 +79,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    new MatchTimer().schedule(); //a match time reporter for the telop portion of the match
   }
 
   /** This function is called periodically during operator control. */
@@ -98,7 +100,7 @@ public class Robot extends TimedRobot {
   @Override
   public void simulationInit() {
     RobotContainer.RC().drivetrain.simulationInit();
-    RobotContainer.RC().photonVision.simulationInit();
+    //RobotContainer.RC().photonVision.simulationInit();  //causing NPE
   }
 
   /** This function is called periodically whilst in simulation. */
---
+++ src/main/java/frc/robot/commands/JoystickRumbleEndless.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.hid.HID_Xbox_Subsystem;
+import frc.robot.Constants.DriverControls.Id;
+
+public class JoystickRumbleEndless extends CommandBase {
+  /** Rumbless while scheduled. */
+
+  HID_Xbox_Subsystem dc;
+  Id id;
+
+  public JoystickRumbleEndless(Id id) {
+    dc = RobotContainer.RC().dc;
+    this.id = id;
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.RC().dc.turnOnRumble(id);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.RC().dc.turnOffRumble(id);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
