b139a2deea64581d84fe462a4fd36a4b74889cea
nathanaelren <58539761+nathanaelren@users.noreply.github.com>
2023-02-21 19:44:36 -0600

    added cmd to directly set blink if so pleased can be bound to button

---
+++ src/main/java/frc/robot/commands/ContinuousBlink.java
@@ -0,0 +1,31 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.BlinkyLights.BlinkyLightUser;
+
+public class ContinuousBlink extends CommandBase implements BlinkyLightUser {
+
+    private final boolean blink;
+    private final Color8Bit color;
+
+    public ContinuousBlink(Color8Bit color, Boolean blink) {
+        this.blink = blink;
+        this.color = color;
+    }
+
+    @Override
+    public boolean requestBlink() {
+        return blink;
+    }
+
+    @Override
+    public Color8Bit colorProvider() {
+        return color;
+    }
+
+    @Override
+    public void initialize() {
+        enableLights();
+    }
+}
