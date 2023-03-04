2f826671f9c6dbc4e45d6874e48e2fb07fba61a9
Gavin <gavin.r.longley@gmail.com>
2023-03-04 10:37:20 -0600

    Got down the Cases for placing peices/ buttons, more work required.

--- src/main/java/frc/robot/commands/Arm/Place.java
+++ src/main/java/frc/robot/commands/Arm/Place.java
@@ -5,70 +5,100 @@
 package frc.robot.commands.Arm;
 
 import frc.robot.commands.Arm.ArmMoveTo;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.flipper;
+import frc.robot.subsystems.ColorSensors;
+import frc.robot.subsystems.ColorSensors.GamePiece;
 
 public class Place extends CommandBase {
   //TODO swich to Enums, dont worry Mr. L
-  enum isCube{
-    cube,
-    cone
-  } 
-  enum isLeft{
+  enum horizontal{
     left,
     right,
     center
   }
-  enum isMiddle{
+  enum vertical{
     middle,
-    top
-  }
-  public boolean needsFlip;
-  public double armExtend;
-  private flipper flipper;
-  /** Creates a new Place. */
-  public Place() {
-    // Use addRequirements() here to declare subsystem dependencies.
+    top,
+    bottom
   }
 
+  public double armExtend;
+  public ColorSensors sensors;
+  /** Creates a new Place. */
+  private horizontal horizontalRequest;
+  private vertical verticalRequest;
+  public Place(ColorSensors sensors,horizontal horizontalRequest, vertical verticalRequest) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.horizontalRequest = horizontalRequest;
+    this.verticalRequest = verticalRequest;
+    this.sensors = sensors;
+  }
+  
+  private void Cone(boolean needsFlip) {
+    switch (horizontalRequest){
+      case left:
+      //drive train move left 14.876 rotations left JASON!!
+      case right: 
+      //drive train move left 14.876 rotations right
+    }
+    //TODO depending on how arm clears, we may want to stall this until the above is finished
+    switch (verticalRequest) {
+      case top: 
+      //arm move out to desired points (top) JASON!!
+      case middle:
+      //arm move out to desired points (middle) JASON!!
+    }
+  }
+  private void Cube() {
+    switch (verticalRequest){
+      case top: 
+      //arm move out to desired points (top center) JASON!!
+      case middle:
+      //arm move out to desired points (middle center) JASON!!
+    }
+  }
+  private void Bottom() {
+    //TODO place bottom. Flip 180?
+    switch (horizontalRequest) {
+      case left:
+
+      case center:
+
+      case right:
+
+    }
+  }
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    //TODO Drive TO starting point
-    //TODO pick up peice from carwash + move elbow to know pos
+  public void initialize() {
     //Next move robot to placement position based on cube/isLeft
-    switch(isCube) {
-      case cube: 
-        //oreant robot
+    //placeholder: Check which color/orentation of peice
+    GamePiece piece = sensors.getGamePiece();
+    if (verticalRequest == vertical.bottom) {
+
     }
-    if (isCube = cube) {
-      //orent robot
-    } if (isCube == false) {
-      if (isLeft == true) {
-        //Robot go left
-      } if (isLeft == false) {
-        //robot go right
-      }
+    switch (piece) {
+      case Cube:
+        Cube();
+      case ConeFacingBack:
+        Cone(false); //TODO: CHECK assumption
+      case ConeFacingFront:
+        Cone(true); //TODO: Check assumption
+      case None:
+        
     }
+
     //Move arns/ elbow out to position, pull from constants
     //Flip flipper if needsFlip is true
     //Move wrist to 180 - theta, taken care of by armMoveTo
     //parallel command
-
-    if (isMiddle == true) {
-      armExtend = Double.NaN; //TODO pull from constants
-    } else {
-      armExtend = Double.NaN; //TODO pull from constants
-    }  
-  ArmMoveTo(armExtend,Double.NaN);
-  flipper.invert();
-
   }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
