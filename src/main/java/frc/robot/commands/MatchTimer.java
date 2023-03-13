79bae4679dcb2d989df4d15384707729d5d10215
DerekL <derek@adminnet.us>
2023-03-13 15:32:14 -0500

    format/imports post rebase onto stl

--- src/main/java/frc/robot/commands/MatchTimer.java
+++ src/main/java/frc/robot/commands/MatchTimer.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.DriverControls.Id;
 
--- src/main/java/frc/robot/util/RobotSpecs.java
+++ src/main/java/frc/robot/util/RobotSpecs.java
@@ -13,118 +13,120 @@ import frc.robot.Constants.WheelOffsets;
 public class RobotSpecs {
 
     public enum RobotNames {
-        SwerveBot("SwerveBot"), 
+        SwerveBot("SwerveBot"),
         CompetitionBot2023("CompetitionBot2023"),
         ChadBot("ChadBot"),
         UnknownBot("UnknownBot"),
         BotOnBoard("BotOnBoard");
+
         String name;
-    
+
         private RobotNames(String name) {
             this.name = name;
         }
-    
+
         public String toString() {
             return name;
         }
-      }
-    
-    
+    }
+
     public RobotNames myRobotName;
     private WheelOffsets myWheelOffsets;
     private ChassisConfig myChassisConfig;
     private SubsystemConfig mySubsystemConfig;
 
-    //CANT DO THIS YET
-    //DriverStation.Alliance color;
-    //private boolean isBlue;
-    
     public RobotSpecs() {
-        this(System.getenv("serialnum"));        
+        this(System.getenv("serialnum"));
     }
-    
-    public RobotSpecs(String serialNo){
+
+    public RobotSpecs(String serialNo) {
         myRobotName = getRobotName(serialNo);
 
-        //if we are simulated, use the competionBot so we have everything
+        // if we are simulated, use the competionBot so we have everything
         if (RobotBase.isSimulation()) {
-            myRobotName = RobotNames.BotOnBoard;//CompetitionBot2023;
+            myRobotName = RobotNames.CompetitionBot2023;
         }
-        //setup to handle any swerve both we have
-        switch(myRobotName){
+        // setup to handle any swerve both we have
+        switch (myRobotName) {
             case SwerveBot:
                 myWheelOffsets = Constants.DriveTrain.swerveBotOffsets;
                 myChassisConfig = Constants.DriveTrain.swerveBotChassisConfig;
-                mySubsystemConfig = Constants.swerveBotSubsystemConfig;               
+                mySubsystemConfig = Constants.swerveBotSubsystemConfig;
                 break;
 
             case CompetitionBot2023:
                 myWheelOffsets = Constants.DriveTrain.compBotOffsets;
                 myChassisConfig = Constants.DriveTrain.compBotChassisConfig;
-                mySubsystemConfig = Constants.compBotSubsystemConfig;            
+                mySubsystemConfig = Constants.compBotSubsystemConfig;
                 break;
-            
+
             case ChadBot:
                 myWheelOffsets = Constants.DriveTrain.chadBotOffsets;
                 myChassisConfig = Constants.DriveTrain.chadBotChassisConfig;
-                mySubsystemConfig = Constants.chadBotSubsystemConfig;              
+                mySubsystemConfig = Constants.chadBotSubsystemConfig;
                 break;
 
             default:
-            case UnknownBot: 
+            case UnknownBot:
             case BotOnBoard:
                 myWheelOffsets = Constants.DriveTrain.swerveBotOffsets;
                 myChassisConfig = Constants.DriveTrain.swerveBotChassisConfig;
-                mySubsystemConfig = Constants.swerveBotSubsystemConfig;                
+                mySubsystemConfig = Constants.swerveBotSubsystemConfig;
                 System.out.println("***Non-driving robot,don't expect too much***");
                 break;
         }
-        System.out.println("***I am " + myRobotName+" ***");
+        System.out.println("***I am " + myRobotName + " ***");
 
-       /* Can't check Team Alliance yet, STILL CONSTRUCTING ROBOT
-        //getting team Color
-        color = DriverStation.getAlliance();
-        if(color == DriverStation.Alliance.Blue){
-            isBlue = true;
-            System.out.println("***We are on blue alliance***");
-        }
-        else {
-        isBlue = false;
-        System.out.println("***We are on red alliance***");
-        */
+        /*
+         * Can't check Team Alliance yet, STILL CONSTRUCTING ROBOT
+         * //getting team Color
+         * color = DriverStation.getAlliance();
+         * if(color == DriverStation.Alliance.Blue){
+         * isBlue = true;
+         * System.out.println("***We are on blue alliance***");
+         * }
+         * else {
+         * isBlue = false;
+         * System.out.println("***We are on red alliance***");
+         */
     }
 
-  public WheelOffsets getWheelOffset(){
-      return myWheelOffsets;
-  }
+    public String getRobotNameString() {
+        return this.myRobotName.name;
+    }
 
-  public ChassisConfig getChassisConfig(){
-      return myChassisConfig;
-  }
+    public WheelOffsets getWheelOffset() {
+        return myWheelOffsets;
+    }
 
-  public SubsystemConfig getSubsystemConfig(){
-      return mySubsystemConfig;
-  }
-  
-  //takes the roborio serial # and returns the robot name
-  public RobotNames getRobotName(String serialNo){
-    RobotNames tempRobotName;
+    public ChassisConfig getChassisConfig() {
+        return myChassisConfig;
+    }
 
-    if (serialNo == null) 
-        return RobotNames.UnknownBot;
+    public SubsystemConfig getSubsystemConfig() {
+        return mySubsystemConfig;
+    }
 
-    if (serialNo.compareTo("031b7511")==0)
-        tempRobotName = RobotNames.SwerveBot;
-    else if (serialNo.compareTo("03238151")==0)
-        tempRobotName = RobotNames.ChadBot;
-    else if (serialNo.compareTo("0312db1a")==0)
-        tempRobotName = RobotNames.BotOnBoard;
-    else if (serialNo.compareTo("032381BF")==0)
-        tempRobotName = RobotNames.CompetitionBot2023;
-    else tempRobotName = RobotNames.UnknownBot;
+    // takes the roborio serial # and returns the robot name
+    public RobotNames getRobotName(String serialNo) {
+        RobotNames tempRobotName;
 
-    System.out.println("***RoboRio SERIAL NUM: " + serialNo);
-    System.out.println("***Robot identified as: " + tempRobotName);
-    return tempRobotName;
-  }
+        if (serialNo == null)
+            return RobotNames.UnknownBot;
+
+        if (serialNo.compareTo("031b7511") == 0)
+            tempRobotName = RobotNames.SwerveBot;
+        else if (serialNo.compareTo("03238151") == 0)
+            tempRobotName = RobotNames.ChadBot;
+        else if (serialNo.compareTo("0312db1a") == 0)
+            tempRobotName = RobotNames.BotOnBoard;
+        else if (serialNo.compareTo("032381BF") == 0)
+            tempRobotName = RobotNames.CompetitionBot2023;
+        else
+            tempRobotName = RobotNames.UnknownBot;
+
+        System.out.println("***RoboRio SERIAL NUM: " + serialNo);
+        System.out.println("***Robot identified as: " + tempRobotName);
+        return tempRobotName;
+    }
 }
