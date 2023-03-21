53e7675ace973bca6fb0b6525e8a700aef16c685
Beast Software Dev <beastsoftwaredev@gmail.com>
2023-03-20 22:47:04 -0500

    RC with closeclaw/w gate

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -28,6 +29,7 @@ import frc.robot.commands.Arm.ArmMoveAtSpeed;
 import frc.robot.commands.Arm.MoveCollectiveArm;
 import frc.robot.commands.Arm.MoveCollectiveArm.CollectiveMode;
 import frc.robot.commands.Automation.CenterTapeSkew;
+import frc.robot.commands.EndEffector.CloseClawWithGate;
 import frc.robot.commands.EndEffector.WheelsIn;
 import frc.robot.commands.EndEffector.WheelsOut;
 import frc.robot.commands.Intake.Washer.DeployIntake;
@@ -320,7 +322,10 @@ public class RobotContainer {
         // HorizontalScoringLane.Right, VerticalScoringLane.Bottom));
 
         //MONDAY TESTING 3/20/23  TODO REMOVE BEFORE COMP
-        dc.Operator().povUp().onTrue(new MoveCollectiveArm(CollectiveMode.pickupShelfFS));
+        dc.Operator().povUp().onTrue(new ParallelCommandGroup(
+            new MoveCollectiveArm(CollectiveMode.pickupShelfFS),
+            new CloseClawWithGate()));
+
         dc.Operator().povDown().onTrue(new MoveCollectiveArm(CollectiveMode.placeConeHighFS));
         dc.Operator().povLeft().onTrue(new SequentialCommandGroup(
             new MoveCollectiveArm(CollectiveMode.travelFS),       //trackFS
