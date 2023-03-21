9e84cb9cf7cb8c2c2c44746274d702a831727e4e
nathanaelren <nren2013@gmail.com>
2023-03-21 16:36:39 -0500

    updated place w/ arm / sdt mvmts

--- src/main/java/frc/robot/commands/Placement/Place.java
+++ src/main/java/frc/robot/commands/Placement/Place.java
@@ -7,8 +7,10 @@ package frc.robot.commands.Placement;
 import com.pathplanner.lib.PathConstraints;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -17,29 +19,35 @@ import frc.robot.Constants.VerticalScoringLane;
 import frc.robot.RobotContainer;
 import frc.robot.commands.Arm.MoveCollectiveArm;
 import frc.robot.commands.Arm.MoveCollectiveArm.CollectiveMode;
+import frc.robot.commands.EndEffector.WheelsIn;
 import frc.robot.commands.Intake.Washer.outtakeCompetitionToggle;
 import frc.robot.commands.auto.goToScoringPosition;
 import frc.robot.commands.swerve.RotateTo;
 import frc.robot.subsystems.Claw_Substyem;
 import frc.robot.subsystems.ColorSensors;
+import frc.robot.subsystems.SwerveDrivetrain;
 import frc.robot.subsystems.ColorSensors.GamePiece;
 import frc.robot.subsystems.hid.HID_Xbox_Subsystem;
-
 
 public class Place extends CommandBase {
 
   // SSs
   private HID_Xbox_Subsystem dc = RobotContainer.RC().dc;
   public ColorSensors colorSensors = RobotContainer.RC().colorSensors;
+  private SwerveDrivetrain sdt = RobotContainer.RC().drivetrain;
+  private Claw_Substyem claw = RobotContainer.RC().claw;
 
-  // deadzone
+  // constants
   final double DEADZONE = 0; // [percent]
+  final double SPEED_MOVE = 0.5; // [m/s] speed moving to / from target
+  final double TIME_MOVE = 1.0; // [s] time to move to / from target
+  final double TIME_DROP = 0.5; // [s] time to wait after claw opens / wheels spin out before going back
 
   // state vars
   private HorizontalScoringLane horizontalRequest;
   private VerticalScoringLane verticalRequest;
   private GamePiece piece;
-  private Claw_Substyem claw;
+  private Command dropCmd;
 
   // the cmd
   private SequentialCommandGroup cmd = new SequentialCommandGroup();
@@ -47,19 +55,20 @@ public class Place extends CommandBase {
   /**
    * This class acts as a command factory.
    * 
-   * Constructs and schedules a new Place command. Encompasses sdt movement, arm extension / retraction.
+   * Constructs and schedules a new Place command. Encompasses sdt movement, arm
+   * extension / retraction.
    * Assumes piece is already in claw (this should be done upon picking up).
    * 
    * @param horizontalRequest
    * @param verticalRequest
    */
-  public Place(HorizontalScoringLane horizontalRequest, VerticalScoringLane verticalRequest) {
+  public Place(HorizontalScoringLane horizontalRequest, VerticalScoringLane verticalRequest, GamePiece piece) {
     this.horizontalRequest = horizontalRequest;
     this.verticalRequest = verticalRequest;
+    this.piece = piece;
 
-    addRequirements(dc, colorSensors);
+    addRequirements(dc);
   }
-
 
   // Called when the command is initially scheduled.
   @Override
@@ -68,27 +77,43 @@ public class Place extends CommandBase {
     move();
 
     // 2. Move arm out
-    piece = colorSensors.getCurrentGamePiece();
-
     switch (piece) {
       case Cube:
         Cube();
+        dropCmd = new WheelsIn().withTimeout(TIME_DROP);
         break;
       case ConeFacingBack:
         ConeBack();
+        dropCmd = new InstantCommand(() -> {
+          claw.open();
+        }).withTimeout(TIME_DROP);
         break;
       case ConeFacingFront:
         ConeFront();
+        dropCmd = new InstantCommand(() -> {
+          claw.open();
+        }).withTimeout(TIME_DROP);
         break;
       case None:
         break;
-      }
+    }
 
-    //MrL's comments: 3/13/23, this is pretty complicated, looks like a simple math.abs() would be good enough?
-    // nren 3/16/23: I mean it's just finding the magnitude of the vector of the stick in 2d space, but I think consistency is important so can't just check one axis
+    // 3. Move above target, place, moves back
+    MovePlace();
+
+    // 4. Go to travel position
+    Retract();
+
+    // MrL's comments: 3/13/23, this is pretty complicated, looks like a simple
+    // math.abs() would be good enough?
+    // nren 3/16/23: I mean it's just finding the magnitude of the vector of the
+    // stick in 2d space, but I think consistency is important so can't just check
+    // one axis
     cmd.until(() -> {
-      boolean xStickStill = (Math.sqrt(Math.pow(dc.Driver().getLeftX(), 2) + Math.pow(dc.Driver().getLeftY(), 2)) > DEADZONE); 
-      boolean yStickStill = (Math.sqrt(Math.pow(dc.Driver().getRightX(), 2) + Math.pow(dc.Driver().getRightY(), 2)) > DEADZONE);
+      boolean xStickStill = (Math
+          .sqrt(Math.pow(dc.Driver().getLeftX(), 2) + Math.pow(dc.Driver().getLeftY(), 2)) > DEADZONE);
+      boolean yStickStill = (Math
+          .sqrt(Math.pow(dc.Driver().getRightX(), 2) + Math.pow(dc.Driver().getRightY(), 2)) > DEADZONE);
       return !(xStickStill && yStickStill);
     }).schedule();
   }
@@ -98,7 +123,7 @@ public class Place extends CommandBase {
    */
   private void move() {
     // 1. move to general vicinity
-    cmd.addCommands(new goToScoringPosition(new PathConstraints(2,3), horizontalRequest));
+    cmd.addCommands(new goToScoringPosition(new PathConstraints(2, 3), horizontalRequest));
 
     // 2. correct for OTF path generation rotation error
     // untested below
@@ -117,13 +142,12 @@ public class Place extends CommandBase {
    */
   private void MidHigh(CollectiveMode armLocation) {
     cmd.addCommands(
-      new MoveCollectiveArm(armLocation),
-      // need to test close
-      new InstantCommand(() -> {
-        claw.close();
-      }),
-      new MoveCollectiveArm(CollectiveMode.travelFS)
-    );
+        new MoveCollectiveArm(armLocation),
+        // need to test close
+        new InstantCommand(() -> {
+          claw.close();
+        }),
+        new MoveCollectiveArm(CollectiveMode.travelFS));
   }
 
   /**
@@ -131,12 +155,12 @@ public class Place extends CommandBase {
    */
   private void ConeBack() {
     switch (verticalRequest) {
-      case Top: 
-        MidHigh(CollectiveMode.placeConeHighFS); //TODO cone/cube customization in enum
+      case Top:
+        MidHigh(CollectiveMode.placeConeHighFS); // TODO cone/cube customization in enum
         break;
       case Middle:
-        MidHigh(CollectiveMode.placeConeMidFS); //TODO cone/cube customization in enum
-        break;    
+        MidHigh(CollectiveMode.placeConeMidFS); // TODO cone/cube customization in enum
+        break;
       case Bottom:
         Bottom();
         break;
@@ -147,51 +171,88 @@ public class Place extends CommandBase {
    * Constructs pacing cmd based on object being a cone facing forward.
    */
   private void ConeFront() {
-  switch (verticalRequest) {
-    case Top: 
-      MidHigh(CollectiveMode.placeConeHighFS); //TODO cone/cube customization in enum
-      break;
-    case Middle:
-      MidHigh(CollectiveMode.placeConeMidFS);  //TODO cone/cube customization in enum
-      break;    
-    case Bottom:
-      Bottom();
-      break;
-  }
-}
-
-  /**
-   * Constructs placing cmd based on object being a cube.
-   */
-  private void Cube() {  
     switch (verticalRequest) {
-      case Top: 
-        MidHigh(CollectiveMode.placeCubeHighFS); //TODO cone/cube customization in enum
+      case Top:
+        MidHigh(CollectiveMode.placeConeHighFS); // TODO cone/cube customization in enum
         break;
       case Middle:
-        MidHigh(CollectiveMode.placeCubeMidFS);  //TODO cone/cube customization in enum
-        break;    
+        MidHigh(CollectiveMode.placeConeMidFS); // TODO cone/cube customization in enum
+        break;
       case Bottom:
         Bottom();
         break;
     }
   }
-  
+
+  /**
+   * Constructs placing cmd based on object being a cube.
+   */
+  private void Cube() {
+    switch (verticalRequest) {
+      case Top:
+        MidHigh(CollectiveMode.placeCubeHighFS); // TODO cone/cube customization in enum
+        break;
+      case Middle:
+        MidHigh(CollectiveMode.placeCubeMidFS); // TODO cone/cube customization in enum
+        break;
+      case Bottom:
+        Bottom();
+        break;
+    }
+  }
+
+  /**
+   * Moves to / from the target with arm extended and places the piece on the
+   * target
+   */
+  private void MovePlace() {
+    cmd.addCommands(
+      // 1. Move to
+        new InstantCommand(() -> {
+          sdt.drive(new SwerveModuleState[] {
+              new SwerveModuleState(SPEED_MOVE, new Rotation2d()),
+              new SwerveModuleState(SPEED_MOVE, new Rotation2d()),
+              new SwerveModuleState(SPEED_MOVE, new Rotation2d()),
+              new SwerveModuleState(SPEED_MOVE, new Rotation2d())
+          });
+        }).withTimeout(TIME_MOVE),
+
+      // 2. Drop piece
+        dropCmd,
+        
+      // 3. Move back
+        new InstantCommand(() -> {
+          sdt.drive(new SwerveModuleState[] {
+              new SwerveModuleState(-SPEED_MOVE, new Rotation2d()),
+              new SwerveModuleState(-SPEED_MOVE, new Rotation2d()),
+              new SwerveModuleState(-SPEED_MOVE, new Rotation2d()),
+              new SwerveModuleState(-SPEED_MOVE, new Rotation2d())
+          });
+        }).withTimeout(TIME_MOVE));
+  }
+
+  /**
+   * Retracts piece
+   */
+  private void Retract() {
+    cmd.addCommands(new MoveCollectiveArm(CollectiveMode.travelFS), new MoveCollectiveArm(CollectiveMode.travelLockFS));
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() { 
+  public void execute() {
     // do nothing
   }
-  
+
   @Override
   public void end(boolean interrupted) {
     // do nothing
   }
-  
+
   @Override
   public boolean isFinished() {
-    // it's movement cmd path factory, should be done after init / upon first time execute is called
+    // it's movement cmd path factory, should be done after init / upon first time
+    // execute is called
     return true;
   }
 }
