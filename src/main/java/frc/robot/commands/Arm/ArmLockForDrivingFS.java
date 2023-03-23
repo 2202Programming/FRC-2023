79235be36d24cc072377709ad17562185ee18263
Beast Software <beastsoftwaredev@gmail.com>
2023-03-23 18:50:02 -0500

    speeeeeeeeeed (on home)

--- src/main/java/frc/robot/commands/Arm/ArmLockForDrivingFS.java
+++ src/main/java/frc/robot/commands/Arm/ArmLockForDrivingFS.java
@@ -32,7 +32,7 @@ public class ArmLockForDrivingFS extends CommandBase {
   Command cmd;  //this is what will run
 
   Positions arm_current;
-  double ELBOW_RETRACT = 60.0; // [deg/s] use slower speed
+  double ELBOW_RETRACT = 120.0; // [deg/s] use slower speed
   double ARM_RETRACT = 16.0;
   double FLIP_TIME = 2.0; // [s] for waiting when flip in safe area
   Positions safe = CollectivePositions.safeToFlip.pos_info;
