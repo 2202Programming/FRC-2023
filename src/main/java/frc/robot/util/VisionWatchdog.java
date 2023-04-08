02705235b741d62d10cdac005efd654803945f7b
Jason Rubenstein <jruben4@yahoo.com>
2023-04-08 09:48:52 -0500

    vision watchdog NT

--- src/main/java/frc/robot/util/VisionWatchdog.java
+++ src/main/java/frc/robot/util/VisionWatchdog.java
@@ -5,6 +5,9 @@
 package frc.robot.util;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 
 /** Add your docs here. */
@@ -13,6 +16,12 @@ public class VisionWatchdog {
     private Timer timer;
     private double lastUpdateTime;
     private double updateInterval;
+    private NetworkTable table;
+    private NetworkTableEntry nt_diffX;
+    private NetworkTableEntry nt_diffY;
+    private NetworkTableEntry nt_updateInterval;
+    
+    public final String NT_Name = "Vision"; // expose data under Vision table
 
     /* VisionWatchdog tracks time since it's last update was called, and only prints if the time interval has been longer than defined
      * Prevents hammering prints for every vision update
@@ -20,14 +29,22 @@ public class VisionWatchdog {
     public VisionWatchdog(double updateInterval){
         timer.start();
         this.updateInterval = updateInterval;
+        table = NetworkTableInstance.getDefault().getTable(NT_Name);
+        nt_diffX = table.getEntry("/X Diff");
+        nt_diffY = table.getEntry("/Y Diff");
+        nt_updateInterval = table.getEntry("/Update Interval");
     }
 
     public void update(Pose2d currentPose, Pose2d lastPose){
         double currentTime = timer.get();
         double timeDiff = currentTime-lastUpdateTime;
+        double xDiff = Math.abs(currentPose.getX() - lastPose.getX());
+        double yDiff = Math.abs(currentPose.getY() - lastPose.getY());
+        nt_diffX.setDouble(xDiff);
+        nt_diffY.setDouble(yDiff);
+        nt_updateInterval.setDouble(timeDiff);
+
         if(timeDiff > updateInterval){
-            double xDiff = Math.abs(currentPose.getX() - lastPose.getX());
-            double yDiff = Math.abs(currentPose.getY() - lastPose.getY());
             System.out.println("***Vision Pose Update - " + timeDiff + "s since last update.  X,Y Diff = ("+xDiff+","+yDiff+").");
         }
         lastUpdateTime = currentTime;
